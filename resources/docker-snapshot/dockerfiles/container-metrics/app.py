2d04d34ad7fe831c39ad4c1363910ab9cca0e676
Mike Haller <mike.haller@bosch.com>
2023-02-08 16:18:44 +0000

    WIP: Container Metrics
    Signed-off-by: GitHub <noreply@github.com>

--- resources/docker-snapshot/dockerfiles/container-metrics/app.py
+++ resources/docker-snapshot/dockerfiles/container-metrics/app.py
@@ -18,6 +18,9 @@ import json
 import re
 import logging
 import sys
+import time
+import datetime
+import requests
 
 appname = "kanto2prometheus"
 
@@ -37,7 +40,11 @@ app.config['MQTT_PASSWORD'] = ''
 app.config['MQTT_KEEPALIVE'] = 5
 app.config['MQTT_TLS_ENABLED'] = False
 
-topic = '+/e/dummy-tenant-id/dummy-namespace:dummy-gateway:dummy-device-id:edge:containers'
+app.config['LOKI_URL'] = 'http://loki.leda-network:3100'
+#app.config['LOKI_URL'] = 'http://localhost:3100'
+
+# topic = '+/e/dummy-tenant-id/dummy-namespace:dummy-gateway:dummy-device-id:edge:containers'
+topic = '#'
 
 prometheus_metric_prefix = "leda_container"
 
@@ -65,23 +72,69 @@ def on_unsubscribe(client, userdata, mid):
 def on_disconnect():
     log.warning('Disconnected from MQTT')
 
+def lokiSend(deviceId,containerId,message):
+    url = app.config['LOKI_URL'] + '/loki/api/v1/push'
+    curr_datetime = time.time_ns()
+    headers = {
+        'Content-type': 'application/json'
+    }
+    payload = {
+            "streams": [
+                        {
+                            "stream": 
+                                {
+                                    "deviceId": deviceId,
+                                    "containerId": containerId
+                                 },
+                            "values": [ 
+                                [ str(curr_datetime), message ] 
+                            ]
+                        }
+                       ]
+    }
+    payload = json.dumps(payload)
+    try:
+        answer = requests.post(url, data=payload, headers=headers)
+        # log.info("Answer from Loki: %s",answer)
+    except Exception as e:
+        log.warning('Error sending to loki %s:',url, e)
+
 def parseContainerState(topic,data):
     # data[path] = /features/Container:95aaa60b-4b1f-40f5-b7c6-8fb4a51510c8/properties/status/state
     # print("parsing",data)
     
-    if "com.bosch.iot.suite.edge.containers:Container:1.5.0" in data['value']['definition']:
-        splitted = data['path'].split('/')
-        container_with_prefix=splitted[2]
-        splitted2 = container_with_prefix.split(":")
-        containerId=splitted2[1]
-        containerName=data['value']['properties']['status']['name']
-        containerNames[containerId]=containerName
-        log.info("Received container name: %s = %s", containerId, containerName)
-    elif "com.bosch.iot.suite.edge.containers:ContainerFactory:1.3.0" in data['value']['definition']:
-        # Skippable
-        return
+    tenantId=topic.split("/")[2]
+    # "topic": "dummy-namespace/dummy-gateway:dummy-device-id:edge:containers/things/live/messages/data",
+    topic_split = data['topic'].split('/')
+    # dummy-namespace
+    topic_namespace = topic_split[0]
+    # dummy-gateway:dummy-device-id:edge:containers
+    topic_full_device_identifier = topic_split[1]
+    deviceIds=topic_full_device_identifier.split(":")
+    deviceId=deviceIds[0] + ":" + deviceIds[1]
+    
+    splitted = data['path'].split('/')
+    container_with_prefix=splitted[2]
+    splitted2 = container_with_prefix.split(":")
+    containerId=splitted2[1]
+    
+    if 'definition' in data['value']:
+        if "com.bosch.iot.suite.edge.containers:Container:1.5.0" in data['value']['definition']:
+            containerName=data['value']['properties']['status']['name']
+            containerNames[containerId]=containerName
+            log.info("Received container name: %s = %s", containerId, containerName)
+            lokiSend(deviceId, containerId,"Received container name " + containerName)
+        elif "com.bosch.iot.suite.edge.containers:ContainerFactory:1.3.0" in data['value']['definition']:
+            # Skippable
+            return
+        else:
+            log.warning("Ignoring unknown container message: %s",data['value']['definition'])
+            lokiSend(deviceId, containerId,"Ignoring unknown message")
+    elif data['topic'].endswith("edge:containers/things/twin/commands/modify"):
+        log.info("Container %s status changed to %s", containerId, data['value']['status'])
+        lokiSend(deviceId, containerId,"Container changed status to " + data['value']['status'])
     else:
-        log.warning("Ignoring unknown container message: %s",data['value']['definition'])
+        log.warning("Message does not have a definition: %s",data['value'])
 
 def parseMetrics(topic,data):
     tenantId=topic.split("/")[2]
@@ -93,7 +146,9 @@ def parseMetrics(topic,data):
     topic_full_device_identifier = topic_split[1]
     deviceIds=topic_full_device_identifier.split(":")
     deviceId=deviceIds[0] + ":" + deviceIds[1]
-    
+
+    log.info("Received metrics for device: %s", deviceId)
+
     snapshots = data['value']['snapshot']
     for snapshot in snapshots:
             originator = snapshot['originator']
@@ -116,33 +171,39 @@ def parseMetrics(topic,data):
                 prom_label = "containerId=\"{}\", tenantId=\"{}\", deviceId=\"{}\", name=\"{}\"".format(containerId,tenantId,deviceId,containerName)
                 fullname = "{}_{}{{{}}}".format(prometheus_metric_prefix,prom_metric_name,prom_label)
                 
-                prometheus_metrics[fullname]=mValue
+                value_timestamp = int(time.time()) * 1000
+                value_with_timestamp = "{} {}".format(mValue,value_timestamp)
+                prometheus_metrics[fullname]=value_with_timestamp
+
+                lokiSend(deviceId, containerId, "Received metrics for device " + deviceId )
+
 
 @mqtt_client.on_message()
 def on_message(client, userdata, message):
     topic=message.topic
-    payload=message.payload.decode()
-    data = json.loads(payload)
     
-    if (data['path'] == "/features/Metrics/outbox/messages/data"):
-        log.info("Received Kanto container metrics update via MQTT")
-        parseMetrics(topic,data)
-        return
-    
-    if (data['path'].startswith("/features/Container")):
-        # /features/Container:95aaa60b-4b1f-40f5-b7c6-8fb4a51510c8/properties/status/state
-        parseContainerState(topic,data)
-        return
-    
-    if (data['path'] == "/features/Metrics"):
-        # Skippable
-        return
-    
-    if (data['path'] == "/features/SoftwareUpdatable"):
-        # Skippable
-        return
-    
-    log.warning("Received Kanto message with unknown path: ",str(data['path']))
+    if topic.endswith(":edge:containers"):
+        payload=message.payload.decode()
+        data = json.loads(payload)
+        
+        if (data['path'] == "/features/Metrics/outbox/messages/data"):
+            parseMetrics(topic,data)
+            return
+        
+        if (data['path'].startswith("/features/Container:")):
+            # /features/Container:95aaa60b-4b1f-40f5-b7c6-8fb4a51510c8/properties/status/state
+            parseContainerState(topic,data)
+            return
+        
+        if (data['path'] == "/features/Metrics"):
+            # Skippable
+            return
+        
+        if (data['path'] == "/features/SoftwareUpdatable"):
+            # Skippable
+            return
+        
+        log.warning("Received Kanto message with unknown path: ",str(data['path']))
 
 @mqtt_client.on_log()
 def handle_logging(client, userdata, level, buf):
@@ -170,4 +231,5 @@ if __name__ == '__main__':
     log.info("Startup of MQTT Client, connecting to %s",app.config['MQTT_BROKER_URL'])
     mqtt_client.init_app(app)
     log.info("Startup of Flask web app")
+    lokiSend('none','none','Hello from leda-container-metrics')
     app.run(debug=False,port=7355,host='0.0.0.0') 
--- resources/docker-snapshot/dockerfiles/leda-initializer-entrypoint.sh
+++ resources/docker-snapshot/dockerfiles/leda-initializer-entrypoint.sh
@@ -17,26 +17,29 @@ mkdir -p ~/.ssh/
 
 function prepareTarget() {
     local TARGET_HOSTNAME=$1
+    local DEVICE_ID=$2
     echo "Preparing ${TARGET_HOSTNAME} ..."
     echo "- Adding SSH Host Key"
     ssh-keyscan -p 2222 -H ${TARGET_HOSTNAME} >> ~/.ssh/known_hosts 2> /dev/null
     echo "- Waiting for container management to become active (running)"
     ssh -p 2222 root@${TARGET_HOSTNAME} systemctl is-active --wait container-management
     echo "- Sending MQTT to simulate cloud connection"
-    mosquitto_pub -h ${TARGET_HOSTNAME} -t "edge/thing/response" -f simulate-cloud-connection.json
+    sleep 5
+    cat simulate-cloud-connection.json | sed "s/dummy-device-id/${DEVICE_ID}/" | mosquitto_pub -h ${TARGET_HOSTNAME} -t "edge/thing/response" -s
     sleep 1
     echo "- Sending MQTT to enable container metrics"
-    mosquitto_pub -h ${TARGET_HOSTNAME} -t "command//dummy-namespace:dummy-gateway:dummy-device-id:edge:containers/req/3fdc463c-293c-4f39-ab19-24aef7944550/request" -f enable-container-metrics.json
+    cat enable-container-metrics.json | sed "s/dummy-device-id/${DEVICE_ID}/" | mosquitto_pub -h ${TARGET_HOSTNAME} -t "command//dummy-namespace:dummy-gateway:${DEVICE_ID}:edge:containers/req/3fdc463c-293c-4f39-ab19-24aef7944550/request" -s
     echo "- Finished"
 }
 
 function verifyTarget() {
     local TARGET_HOSTNAME=$1
+    local DEVICE_ID=$2
     echo "Verifying ${TARGET_HOSTNAME} ..."
     #TOPIC="e/<tenantId>/<gatewayId>:<deviceId>:edge:containers"
-    EXPECTED_TOPIC="e/dummy-tenant-id/dummy-namespace:dummy-gateway:dummy-device-id:edge:containers"
+    EXPECTED_TOPIC="e/dummy-tenant-id/dummy-namespace:dummy-gateway:${DEVICE_ID}:edge:containers"
     # Wait for first fresh (not retained) message on the topic for maximum 10 seconds
-    mosquitto_sub -h ${TARGET_HOSTNAME} -t "${EXPECTED_TOPIC}" -W 10 -C 1 -R -v
+    mosquitto_sub -h ${TARGET_HOSTNAME} -t "${EXPECTED_TOPIC}" -W 20 -C 1 -R -v
     RC=$?
     echo "- Return code: ${RC}"
     if [ $RC = 0 ]; then
@@ -48,11 +51,11 @@ function verifyTarget() {
     fi
 }
 
-prepareTarget leda-x86.leda-network
-prepareTarget leda-arm64.leda-network
+prepareTarget leda-x86.leda-network leda-x86
+prepareTarget leda-arm64.leda-network leda-arm64
 
-verifyTarget leda-x86.leda-network
-verifyTarget leda-arm64.leda-network
+verifyTarget leda-x86.leda-network leda-x86
+verifyTarget leda-arm64.leda-network leda-arm64
 
 echo "Preparation done."
 
