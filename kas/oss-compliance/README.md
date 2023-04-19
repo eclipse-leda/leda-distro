34b5f352e6e38154f25c9df97cee29ceb4879c66
Mike Haller <mike.haller@bosch.com>
2023-04-19 10:30:38 +0000

    kas config updates
    Signed-off-by: GitHub <noreply@github.com>

--- kas/oss-compliance/README.md
+++ kas/oss-compliance/README.md
@@ -5,6 +5,18 @@
 The Eclipse Oniro project provides an integration for IP compliance auditing.
 The tool is called *tinfoilhat` and allows to parse a BitBake build output.
 
+```shell
+# First: Build with generation of source mirrors.
+# Do NOT include remote mirrors (mirrors.yaml)
+kas build kas/leda-qemux86-64.yaml:kas/oss-compliance/tinfoilhat.yaml
+
+# Second: Run tinfoilhat
+cd resources/oniro
+./run-docker.sh
+```
+
+Generated output is in `${WORKSPACE}/build/tmp/deploy/tinfoilhat`
+
 ## Dependency Track
 
 To upload Yocto-generated SPDX information to Dependency Track, an additional meta-layer `meta-dependencytrack` needs to be used.
@@ -37,4 +49,6 @@ popd
 kas build kas/leda-qemux86-64.yaml:kas/oss-compliance/dependencytrack.yaml
 ```
 
+## Double-Open Project
 
+Note: `meta-doubleopen` currently does not support Yocto Kirkstone release and cannot be used.
--- resources/oniro/run-docker.sh
+++ resources/oniro/run-docker.sh
@@ -1,8 +1,25 @@
 #!/bin/sh
+# /********************************************************************************
+# * Copyright (c) 2023 Contributors to the Eclipse Foundation
+# *
+# * See the NOTICE file(s) distributed with this work for additional
+# * information regarding copyright ownership.
+# *
+# * This program and the accompanying materials are made available under the
+# * terms of the Apache License 2.0 which is available at
+# * https://www.apache.org/licenses/LICENSE-2.0
+# *
+# * SPDX-License-Identifier: Apache-2.0
+# ********************************************************************************/
 
 docker build --tag tinfoilhat/tinfoilhat -f Dockerfile .
 
-#    --entrypoint /bin/bash \
+# Run tinfoilhat
+# Note:
+# - BitBake Sanity checker requires that the absolute paths of the TMP_DIR stays the same,
+#   so we need to mount the host path to the same path inside the container and let
+#   the entrypoint script know about them as well, so that i can adapt the PYTHONPATH variable correctly.
 docker run --rm -it \
-    --volume ${CODESPACE_VSCODE_FOLDER}:/workspace \
-    tinfoilhat/tinfoilhat
+    --volume ${CODESPACE_VSCODE_FOLDER}:${CODESPACE_VSCODE_FOLDER} \
+    tinfoilhat/tinfoilhat \
+    "${CODESPACE_VSCODE_FOLDER}" "$(git describe --tags)"
