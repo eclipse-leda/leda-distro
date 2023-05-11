56cb95b92fcbc8842c3da9e9ad1bcb835bd77d84
vasilvas99 <vasil.ivanov3@bosch.io>
2023-05-11 09:25:35 +0300

    Add license header to sbom-converter

--- .github/workflow-scripts/sbom-converter.py
+++ .github/workflow-scripts/sbom-converter.py
@@ -1,5 +1,18 @@
 #!/usr/bin/env python3
 
+# /********************************************************************************
+# * Copyright (c) 2023 Contributors to the Eclipse Foundation
+# *
+# * See the NOTICE file(s) distributed with this work for additional
+# * information regarding copyright ownership.
+# *
+# * This program and the accompanying materials are made available under the
+# * terms of the Apache License 2.0 which is available at
+# * https://www.apache.org/licenses/LICENSE-2.0
+# *
+# * SPDX-License-Identifier: Apache-2.0
+# ********************************************************************************/
+
 import os
 import sys
 import shutil
